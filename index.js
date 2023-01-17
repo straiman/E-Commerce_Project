69d27bc2a71d2f5eea6232cf4a49589ef7eb928a
Samantha Traiman <samantha.traiman+git@gmail.com>
2023-01-17 11:15:38 +0200

    skeleton of routes, db interaction

--- index.js
+++ index.js
@@ -3,17 +3,83 @@ const port = process.env.PORT || 3100;
 const express = require('express');
 
 // custom modules/functions defined in other files can be declared here;
+const {connectToDB, addUser, getProduct, getProducts} = require('./utils/database');
 
 const app = express();
 
+// for any routes that need to parse JSON into objects:
+app.use(express.json());
+
+const db = connectToDB();
+
+// HOME
 app.get('/', (req, res) => {
     res.send('<p>Hello</p>');
 });
 
-// Add other routes/methods here.
+// LOGIN
+app.get('/login', (req, res) => {
+    ;
+});
 
-// remember to put app.use(express.json());
-// before any routes that need to parse JSON into objects
+// REGISTER
+app.post('/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    addUser(username, password);
+});
+
+// PRODUCT GALLERY
+app.get('/products', (req, res) => {
+    ;
+});
+
+// PRODUCT DETAILS
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id;
+});
+
+// CATEGORY
+app.get('/category/:category', (req, res) => {
+    const category = req.params.category;
+});
+
+// SEARCH
+app.get('/search', (req, res) => {
+    const terms = req.query.search;
+});
+
+// CART
+app.get('/cart', (req, res) => {
+    ;
+});
+
+// WISHLIST
+app.get('/wishlist', (req, res) => {
+    ;
+});
+
+// ACCOUNT
+app.get('/account', (req, res) => {
+    ;
+});
+
+// ORDERS
+app.get('/orders', (req, res) => {
+    ;
+});
+
+// ORDER DETAILS
+app.get('/orders/:id', (req, res) => {
+    const id = req.params.id;
+});
+
+// CONTACT US
+app.get('/contact-us', (req, res) => {
+    ;
+});
+
+// Add other routes/methods here.
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
